100c3c6fc5adadc5190f8647635ddeeb8ff0478a
AndresMon <andres.montoyao@autonoma.edu.co>
2025-10-15 20:09:36 -0500

    base de proyecto

---
+++ src/main/java/com/BackendG1/GenomaBankAPI/GenomaBankApiApplication.java
@@ -0,0 +1,13 @@
+package com.BackendG1.GenomaBankAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenomaBankApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenomaBankApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BackendG1/GenomaBankAPI/GenomaBankApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BackendG1.GenomaBankAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenomaBankApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
