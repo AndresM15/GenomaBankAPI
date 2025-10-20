1155fdd4ba1da1d33fda634d88d7c52eaef1fd73
Juan Camilo Pulgarin <juancapulgarin9@gmail.com>
2025-10-19 23:45:04 -0500

    Creamos la entidad Genes y agregamos sus atributos

--- src/main/java/com/BackendG1/GenomaBankAPI/entities/Genes.java
+++ src/main/java/com/BackendG1/GenomaBankAPI/entities/Genes.java
@@ -1,4 +1,29 @@
 package com.BackendG1.GenomaBankAPI.entities;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "Gen")
 public class Genes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "simbolo", nullable = false)
+    private String symbol;
+
+    @Column(name = "posicion_inicio", nullable = false)
+    private Integer startPos;
+
+    @Column(name = "posicion_final", nullable = false)
+    private Integer endPos;
+
+    @Column(name = "orientacion", nullable = false)
+    private String strand;
+
+    @Column(name = "secuencia_adn", columnDefinition = "LONGTEXT", nullable = false)
+    private String sequence;
 }
