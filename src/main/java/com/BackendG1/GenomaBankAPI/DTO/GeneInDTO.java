ec6d58adba2d18b0c28d9e1468d81df8876dff96
Juan Camilo Pulgarin <juancapulgarin9@gmail.com>
2025-10-19 23:50:22 -0500

    Definimos bien los 3 DTOs de forma correcta para que mis métodos GET,POST funcionen correctamente

--- src/main/java/com/BackendG1/GenomaBankAPI/DTO/GeneInDTO.java
+++ src/main/java/com/BackendG1/GenomaBankAPI/DTO/GeneInDTO.java
@@ -1,4 +1,12 @@
 package com.BackendG1.GenomaBankAPI.DTO;
 
+import lombok.Data;
+
+@Data
 public class GeneInDTO {
+    private String symbol;
+    private Integer startPos;
+    private Integer endPos;
+    private String strand;
+    private String sequence;
 }
--- src/main/java/com/BackendG1/GenomaBankAPI/DTO/GeneOutDTO.java
+++ src/main/java/com/BackendG1/GenomaBankAPI/DTO/GeneOutDTO.java
@@ -1,4 +1,13 @@
 package com.BackendG1.GenomaBankAPI.DTO;
 
+import lombok.Data;
+
+@Data
 public class GeneOutDTO {
+    private Long id;
+    private String symbol;
+    private Integer startPos;
+    private Integer endPos;
+    private String strand;
+    private String sequence;
 }
